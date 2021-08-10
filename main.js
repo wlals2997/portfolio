d66e9024e4ff52cc1085a077e24a4796bcd83770
wlals2997 <85855544+wlals2997@users.noreply.github.com>
2021-08-10 18:09:54 +0900

    Js Navbar menu click

--- main.js
+++ main.js
@@ -12,3 +12,16 @@ if(window.scrollY> navbarHeight){
     navbar.classList.remove('navbar--dark');
 }
 });
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) =>{
+const target = event.target;
+const link = target.dataset.link;
+if(link==null) {
+    return;
+}
+console.log(event.target.dataset.link);
+const scrollTo= document.querySelector(link);
+scrollTo.scrollIntoView({behavior:"smooth"});
+});
